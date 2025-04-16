e39ad36dfd8af2840520c764bd50f3ebea8c997a
Ariana Pierro <arianapierro13@gmail.com>
2025-04-15 19:33:54 -0500

    I added constants to my game that may change later

--- src/project.py
+++ src/project.py
@@ -2,6 +2,20 @@ import random
 import pygame
 
 
+pygame.init()
+# Constants
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 650 # Increased height for timer display
+CELL_SIZE = 20
+MAZE_WIDTH = SCREEN_WIDTH // CELL_SIZE
+MAZE_HEIGHT = (SCREEN_HEIGHT - 50) // CELL_SIZE # Adjusted height for timer display
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+GRAY = (192, 192, 192)
+
+
 def main():
     ...
 
