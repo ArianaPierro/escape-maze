161397f19e32b96317942ba91047c059fd329436
Ariana Pierro <arianapierro13@gmail.com>
2025-04-16 10:01:40 -0500

    I added a draw_maze function

--- src/project.py
+++ src/project.py
@@ -20,12 +20,22 @@ def create_maze():
     maze = [[0] * MAZE_WIDTH for _ in range(MAZE_HEIGHT)]
     # Randomly add obstacles
     for _ in range(200):
-        x= random.randint(0, MAZE_WIDTH - 1)
-        y= random.randint(0, MAZE_HEIGHT - 1)
+        x = random.randint(0, MAZE_WIDTH - 1)
+        y = random.randint(0, MAZE_HEIGHT - 1)
         maze[y][x] = 1
     # Set endpoint
     maze[MAZE_HEIGHT - 1][MAZE_WIDTH - 1] = 2
     return maze
+
+# Draw Maze
+def draw_maze(screen, maze):
+    for y in range(MAZE_HEIGHT):
+        for x in range(MAZE_WIDTH):
+            if maze [y][x] == 1:
+                pygame.draw.rect(screen, BLACK, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+            elif maze [y][x] == 2:
+                pygame.draw.rect(screen, RED, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))    
+
 
 def main():
     ...
