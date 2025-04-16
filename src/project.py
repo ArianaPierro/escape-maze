a93e3eb152de0166ebaef2f3d4b6a399f56a936d
Ariana Pierro <arianapierro13@gmail.com>
2025-04-16 15:53:50 -0500

    Added a timer class that I'll have at the bottom of my game to make it more interesting

--- src/project.py
+++ src/project.py
@@ -54,7 +54,30 @@ class Player:
         pygame.draw.rect(screen, GREEN, (self.x * CELL_SIZE, self.y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
 # Timer Class
+class Timer:
 
+    def __init__(self, countdown_time):
+        self.font = pygame.font.SysFont(None, 36)
+        self.start_time = pygame.time.get_ticks()
+        self.countdown_time = countdown_time # Time in seconds
+
+    def get_time(self):
+        elasped_time = pygame.time.get_ticks() - self.start_time
+        remaining_time = self.countdown_time - elasped_time // 1000
+        if remaining_time < 0:
+            remaining_time = 0
+        minutes = remaining_time // 60
+        seconds = remaining_time % 60
+        return f"Time: {minutes:02}:{seconds:02}"
+
+    def draw(self, screen):
+        time_text = self.font.render(self.get_time(), True, BLACK)
+        screen.blit(time_text, (10, 600))
+
+    def is_time_up(self):
+        elasped_time = pygame.time.get_ticks()- self.start_time
+        remaining_time = self.countdown_time - elasped_time // 1000
+        return remaining_time <= 0 
 
 
 def main():
